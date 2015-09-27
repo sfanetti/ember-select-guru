5734e7fbffa809554ce0cdd3517ced29881260c9
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-09-27 08:31:50 +0200

    add lodash to bower modules

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-select-guru'
+  name: 'ember-select-guru',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/lodash/lodash.min.js');
+  }
 };
