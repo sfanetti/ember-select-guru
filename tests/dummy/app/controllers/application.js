02615124e7f9c3e92fd099c2e40ffaedf6a92c79
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-09-28 20:11:53 +0200

    handle selection for multi and single in dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,9 +4,16 @@ const { Controller } = Ember;
 
 export default Controller.extend({
   options: Ember.A(['123', '456']),
+  value: null,
   actions: {
     queryTermChanged(queryTerm) {
       console.log(queryTerm);
+    },
+    handleSingleSelect(option) {
+      this.set('singleValue', option);
+    },
+    handleMultiSelect(options) {
+      this.set('multipleValue', options);
     }
   }
 });
