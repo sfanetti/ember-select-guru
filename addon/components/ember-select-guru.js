01d989489ad27dd926da4a86745034aa61dbbfe8
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-09-28 20:11:39 +0200

    add multi and single value

--- addon/components/ember-select-guru.js
+++ addon/components/ember-select-guru.js
@@ -8,11 +8,14 @@ export default Component.extend({
   isPending: false,
   hasFailed: false,
   queryTerm: null,
+  multiple: false,
   searchPlaceholder: 'Type to search...',
   pendingComponent: 'pending-component',
   failureComponent: 'failure-component',
   optionComponent: 'option-component',
   noOptionsComponent: 'no-options-component',
+  singleValueComponent: 'single-value-component',
+  multiValueComponent: 'multi-value-component',
   hasOptions: computed.notEmpty('options'),
   queryTermObserver: observer('queryTerm', function() {
     Ember.run.once(() => {
@@ -21,21 +24,21 @@ export default Component.extend({
         hasFailed: false
       });
       const result = this.attrs.onSearchInputChange(this.get('queryTerm'));
-      // handle if result is a promise
-      if(result === undefined || result === null) {
+      if(result == null) {
+        // handle if result is undefined (internal search)
         // TODO - perform internal search
       } else if('function' === typeof result.then) {
+        // handle if result is a promise
         this.set('isPending', true);
         result.then(() => {
           this.set('isPending', false);
         }, () => {
           this.set('hasFailed', true);
         });
-      // handle if result is an array (external search)
       } else if(Array.isArray(result)) {
+        // handle if result is an array (external search)
         const options = _.intersection(result, this.get('options'));
         this.set('_options', options);
-      // handle if result is undefined (internal search)
       }
     });
   }),
