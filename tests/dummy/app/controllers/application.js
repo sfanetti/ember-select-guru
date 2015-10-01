9eed7762021e601700f67969827f9677954996a1
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-10-01 15:51:33 +0200

    use objects instead of plain values

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,7 +3,17 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  options: Ember.A(['123', '456']),
+  options: Ember.A([
+    Ember.Object.create({
+      value: '123'
+    }),
+    Ember.Object.create({
+      value: '456'
+    }),
+    Ember.Object.create({
+      value: '758'
+    })
+  ]),
   multipleValue: Ember.A([]),
   value: null,
   actions: {
