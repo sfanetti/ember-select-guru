290274cdb944371fcc4614ec7974af6ad8cf8aa2
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-10-03 16:34:30 +0200

    add empty tests to match before refactoring

---
+++ tests/integration/ember-select-guru-test.js
@@ -0,0 +1,28 @@
+import { test, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('ember-select-guru/components/ember-select-guru',
+  'Integration: ember-select-guru/components/ember-select-guru', { integration: true });
+
+test('it renders options on dropdown opening', function(assert) {
+  assert.expect(0);
+});
+
+test('it renders proper notification when promise returned from query', function(assert) {
+  assert.expect(0);
+});
+
+test('it renders proper notification when promise returned from query fails', function(assert) {
+  assert.expect(0);
+});
+
+test('it renders options when promise from returned query resolves', function(assert) {
+  assert.expect(0);
+});
+
+test('it closes the dropdown on selection', function(assert) {
+  assert.expect(0);
+});
+
+test('it closes the dropdown on remove from multiple selection', function(assert) {
+  assert.expect(0);
+});
---
+++ tests/unit/ember-select-guru-test.js
@@ -0,0 +1,36 @@
+import { test, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('ember-select-guru',
+  'Unit: ember-select-guru');
+
+test('query change executes #onSearchInputChange action', function(assert) {
+  assert.expect(0);
+});
+
+test('if #onSearchInputChange returns null, it sets filtered options except currently selected', function(assert) {
+  assert.expect(0);
+});
+
+test('if #onSearchInputChange returns promise, it gets into pending state', function(assert) {
+  assert.expect(0);
+});
+
+test('if #onSearchInputChange returned promise fulfills, it sets options except currently selected', function(assert) {
+  assert.expect(0);
+});
+
+test('if #onSearchInputChange returned promise fails, it gets into failed state', function(assert) {
+  assert.expect(0);
+});
+
+test('if #onSearchInputChange returns array, it sets intersection of returned array and currently available options', function(assert) {
+  assert.expect(0);
+});
+
+test('value change updates possible options', function(assert) {
+  assert.expect(0);
+});
+
+test('options change updates possible options', function(assert) {
+  assert.expect(0);
+});
