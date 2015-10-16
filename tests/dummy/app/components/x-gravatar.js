69a878c5bc30063c649c658358c66bd08425c45a
Michał Hartwich <michal.hartwich@netguru.pl>
2015-10-16 16:57:41 +0200

    create x-gravatar component

---
+++ tests/dummy/app/components/x-gravatar.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+const { computed, run } = Ember;
+
+export default Ember.Component.extend({
+  imageUrl: 'http://www.gravatar.com/avatar/00000000000000000000000000000000'
+});
