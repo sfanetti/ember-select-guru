399dc8ef499e21422814c84ae904665a93a91edf
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-10-26 16:03:36 +0100

    import from Ember and use shortcuts

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,13 +4,13 @@ import names from 'dummy/example-constants/names';
 import colors from 'dummy/example-constants/colors';
 import numbers from 'dummy/example-constants/numbers';
 
-const { Controller } = Ember;
+const { Controller, get, A, RSVP, run } = Ember;
 
 export default Controller.extend({
-  options: Ember.A(numbers),
-  colorOptions: Ember.A(colors),
-  names: Ember.A(names),
-  multipleValue: Ember.A([]),
+  options: A(numbers),
+  colorOptions: A(colors),
+  names: A(names),
+  multipleValue: A([]),
   actions: {
     queryTermChanged(queryTerm) {
       console.log(queryTerm);
@@ -25,8 +25,8 @@ export default Controller.extend({
       this.set('multipleValue', options);
     },
     remoteQueryTermChanged(queryTerm) {
-      let defer = Ember.RSVP.defer();
-      Ember.run.later(this, function() {
+      let defer = RSVP.defer();
+      run.later(this, function() {
         const results = this._searchForUsers(queryTerm);
         this.set('remoteOptions', results);
         defer.resolve();
@@ -43,7 +43,7 @@ export default Controller.extend({
         return options;
       }
       if(isNaN(number)) {
-        return Ember.A();
+        return A();
       }
       return options.filter((item) => {
         return parseInt(item.get('name'), 10) > number;
@@ -52,7 +52,7 @@ export default Controller.extend({
   },
   _searchForUsers(query) {
     return this.get('names').filter((item) => {
-      return (Ember.get(item, 'name') && (Ember.get(item, 'name').indexOf(query) > -1));
+      return (get(item, 'name') && (get(item, 'name').indexOf(query) > -1));
     });
   }
 });
