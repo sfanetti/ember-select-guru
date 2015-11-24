70eb1f41944f967e4a5d40da00ace28571e5aa77
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-11-24 09:30:39 +0100

    add blueprint to run included hook in app

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 module.exports = {
   name: 'ember-select-guru',
 
-  included: function(app) {
-    if (app.options.emberSelectGuru && app.options.emberSelectGuru.includeCss !== false) {
+  included: function(app, parentAddon) {
+    this._super.included.apply(this, arguments);
+    if (!app.options.emberSelectGuru || app.options.emberSelectGuru.includeCss == true) {
       app.import('vendor/ember-select-guru.css');
     }
   }
